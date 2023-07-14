e4956ebf6a7722456a21d7d6015c7048ae164106
Niklas Cathor <niklas.cathor@gmx.de>
2023-07-14 13:53:46 +0200

    delete unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-// pub mod prelude {
-//     pub use super::twoport::{Chain, ChainNode, Element};
-//     pub use super::document::{Document, Section};
-// }
-
-// pub mod twoport;
-// pub mod document;
-
 pub mod circuit;
 pub mod layout;
 pub mod draw;
